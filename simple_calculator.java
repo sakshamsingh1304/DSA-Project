a404be900265d0fac7c48fbebb1e4a5109806dea
Saksham Kushwaha <118334330+sakshamsingh1304@users.noreply.github.com>
2023-10-11 23:51:30 +0530

    Create simple_calculator.java

---
+++ simple_calculator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        
+        System.out.print("Enter the operatoion:(+, -, *, / ) ");
+        char operator = scanner.next().charAt(0);
+        
+        
+        
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
